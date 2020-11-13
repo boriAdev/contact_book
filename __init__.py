b4e69cbf8b8033f7b86852d46fbe7baf28474125
Oluwabori Ajifa <ajifabori21@gmail.com>
2020-11-13 14:41:08 -0500

    created class and flow

--- __init__.py
+++ __init__.py
@@ -1,4 +1,24 @@
 ''' Contact Book Application'''
 
 if __name__=="__main__":
-	
+
+	'''Application Interface'''
+
+	on = True
+
+	while on:
+		print("-------Contact Book----------")
+		print("Choose an option:")
+		print("C - Create New Contact")
+		print("U - Update a contact")
+		print("D - Delete a Contact")
+		print("B - Backup contacts in the Cloud")
+		print("Q - Quit")
+
+		starter =input(' : ')
+
+
+		'''User ends Application'''
+
+		if starter.upper() == "Q":
+			on = False
---
+++ contact_book.py
@@ -0,0 +1,20 @@
+'''Contains functional '''
+class Contacts():
+
+	def __init__(self):
+		self.first_name = first_name
+		self.middle_name = middle_name
+		self.last_name = last_name
+		self.street_add = street_add
+		self.apt_no = apt_no
+		self.city = city
+		self.state = state
+		self.zipcode = zipcode
+		self.phone_no = phone_no
+
+	''' Method to create contact'''
+
+	def 
+
+
+
