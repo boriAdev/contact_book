81869f7bfc399eb7653665cfe4a01e42491aaa33
Oluwabori Ajifa <ajifabori21@gmail.com>
2020-11-14 13:19:22 -0500

    1. implemented create contact feature 2. in the process on integrating DB (sqlite3) to store contact

--- __init__.py
+++ __init__.py
@@ -1,22 +1,48 @@
 ''' Contact Book Application'''
-from contact_book import *
+from contact_book import Contacts
+import sqlite3
 
 if __name__=="__main__":
+
+	# CREATE AND CONNECT TO DATABASE
+	connection = sqlite3.connect('phonebook.db')
+	cursor = connection.cursor()
+
+	create_table_command = """
+	CREATE TABLE IF NOT EXISTS
+	contacts(id INTEGER PRIMARY KEY, firstname TEXT, lastname TEXT, phone_no INTEGER, email TEXT)
+	"""
+	cursor.execute(create_table_command)
 
 	'''Application Interface'''
 
 	on = True
-
 	while on:
 		print("-------Contact Book----------")
 		print("Choose an option:")
 		print("C - Create New Contact")
 		print("U - Update a contact")
 		print("D - Delete a Contact")
+		print("L - List Contact")
 		print("B - Backup contacts in the Cloud")
 		print("Q - Quit")
 
 		starter =input(' : ')
+
+		''' If user selects C to create contact'''
+
+		if starter.upper() =='C':
+
+			# USER INPUT
+			fname = input('Enter First Name: ')
+			lname = input('Enter Last Name: ')
+			phonenumber = input('Enter Phone Number: ')
+			emailaddress = input('Enter Email Address: ')
+
+			# CREATE NEW CONTACT
+			new_contact = Contacts()
+			new_contact.create_new_contact(fname,lname,phonenumber,emailaddress)
+			# print(new_contact.first_name)
 
 
 		'''User ends Application'''
--- contact_book.py
+++ contact_book.py
@@ -1,12 +1,27 @@
+import sqlite3
+import __init__
+
 '''Contains functional '''
 class Contacts():
 
 	def __init__(self):
-		self.first_name = first_name 
-		self.last_name = last_name
-		self.phone_no = phone_no
-		self.email_add = email_add
+		self.first_name = None
+		self.last_name = None
+		self.phone_no = None
+		self.email_add = None
 
 	''' Method to create contact'''
 
-	def 
+	def create_new_contact(self, fname, lname, phonenumber, emailaddress):
+
+		self.first_name = fname
+		self.last_name = lname
+		self.phone_no = phonenumber
+		self.email_add = emailaddress
+
+		cursor.execute("INSERT INTO contacts VALUES(self.first_name, self.last_name, self.phone_no,  self.email_add)")
+
+	def show_all_contacts(self):
+		cursor.execute("SELECT * FROM contacts")
+		results = cursor.fetchall()
+		print(results)
